e8e21585fe7b223fea6e4b19a9c1a5e376c7010b
Daniel Tong <danielytong@yahoo.ca>
2018-11-28 01:15:46 -0500

    Update string split

--- StringManipulation/stringSplit.cpp
+++ StringManipulation/stringSplit.cpp
@@ -2,17 +2,41 @@
 
 using namespace std;
 
-int main(){
-    string a="A B C";
-    stringstream ss;
-    ss<<a;
-    string token;
-    int counter=0;
-    vector<string> tokens;
-    while(ss>>token){
-        tokens.push_back(token);
+vector<string> split(const string& str, const string& pattern) {
+  vector<string> ret;
+
+  string next = "";
+  string buffer = "";
+  int idx = 0;
+
+  for (int i = 0; i < str.size(); i++) {
+    cout << buffer << " " << next << endl;
+    buffer += str[i];
+    if (pattern[idx] == str[i]) {
+      idx++;
+    } else {
+      next += buffer;
+      buffer = "";
+      idx = 0;
     }
-    for(int i=0; i<tokens.size(); i++){
-        cout<<tokens[i]<<endl;
+
+    if (idx == pattern.size()) {
+      ret.push_back(next);
+      idx = 0;
+      next = "";
+      buffer = "";
+    }
+  }
+
+  ret.push_back(next + buffer);
+
+  return ret;
+}
+int main(){
+    string a="A,,B,C,,D";
+
+    vector<string> tokens = split(a, ",,");
+    for (string token : tokens) {
+      cout << token << endl;
     }
 }
